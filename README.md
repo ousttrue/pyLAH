4a055171497bfb50a97d702daa7e8cacf8e77d50
ousttrue <oustrrue@gmail.com>
2017-12-09 22:03:52 +0900

    update version

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 python Linear Algebra Helper.
 
 # Features
+* python3 typing
 * pure python(without numpy)
-
--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup
 
 
 setup(name='PyLAH',
-        version='1.0',
-        description='pure-python linear algebra helper',
-        author='ousttrue',
-        author_email='ousttrue@gmail.com',
-        url='',
-        py_modules=['lah'],
-        test_suite = 'tests'
-        )
-
+      version='1.1',
+      description='pure-python linear algebra helper',
+      author='ousttrue',
+      author_email='ousttrue@gmail.com',
+      url='',
+      py_modules=['lah'],
+      test_suite='tests')
